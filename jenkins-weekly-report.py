2206053026093fd4fbf10e24d85174b313ea29df
Guo Y.K <hi@guoyk.net>
2020-03-14 14:56:54 +0800

    Update jenkins-weekly-report.py

--- jenkins-weekly-report.py
+++ jenkins-weekly-report.py
@@ -167,7 +167,6 @@ def main():
                 'total': success + not_success,
                 'builds': builds,
             })
-            break
 
     Template(TMPL).stream(
         data=data,
